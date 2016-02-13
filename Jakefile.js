d43e6b0418291d1265dc073c20ee10523c2f25d5
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-13 07:40:11 -0800

    create JakeJS build script

---
+++ Jakefile.js
@@ -0,0 +1,11 @@
+( function() {
+    'use strict';
+
+    desc( 'Default build' );
+    task( 'default', function() {
+        console.log( 'Hello, I am the default task.' );
+
+        console.log( '\n\nBUILD OK' );
+    } );
+
+} )();
