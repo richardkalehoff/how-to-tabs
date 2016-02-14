c5b25d0e2cff85f3652333967f58f0237283d56d
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-13 19:35:06 -0800

    setup automatic server in build script

--- Jakefile.js
+++ Jakefile.js
@@ -8,4 +8,12 @@
         console.log( '\n\nBUILD OK' );
     } );
 
+    desc( 'Run a localhost server' );
+    task( 'run', function() {
+        jake.exec( 'node node_modules/http-server/bin/http-server src',
+            { interactive: true },
+            complete
+        );
+    } );
+
 } )();
