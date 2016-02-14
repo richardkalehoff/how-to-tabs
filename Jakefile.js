ce718cf583c2e76eac2f536ab1849409a1dcda2f
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-13 22:29:23 -0800

    add testing to the Jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -1,12 +1,28 @@
 ( function() {
     'use strict';
 
+    desc( 'Start the Karma server' );
+    task( 'karma', function() {
+        jake.exec( 'node_modules/karma/bin/karma start',
+            { interactive: true },
+            complete
+        );
+    }, { async: true } );
+
     desc( 'Default build' );
-    task( 'default', function() {
+    task( 'default', [ 'test' ], function() {
         console.log( 'Hello, I am the default task.' );
 
         console.log( '\n\nBUILD OK' );
     } );
+
+    desc( 'Run tests' );
+    task( 'test', function() {
+        jake.exec( 'node_modules/karma/bin/karma run',
+            { interactive: true },
+            complete
+        );
+    }, { async: true } );
 
     desc( 'Run a localhost server' );
     task( 'run', function() {
