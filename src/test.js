9b34122d31ae6b93bd05d58c00bc91903d28cd1b
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-13 20:16:30 -0800

    run tests using Mocha

--- src/test.js
+++ src/test.js
@@ -3,7 +3,17 @@
 
     var assert = require( 'chai' ).assert;
 
-    assert.equal( add( 3, 4 ), 7 );
+    describe( 'Addition', function() {
+
+        it( 'adds positive numbers', function() {
+            assert.equal( add( 3, 4 ), 7 );
+        } );
+
+        it( 'uses IEEE 754 floating point', function() {
+            assert.equal( add( 0.1, 0.2 ), 0.30000000000000004 );
+        } );
+
+    } );
 
     function add( a, b ) {
         return a + b;
