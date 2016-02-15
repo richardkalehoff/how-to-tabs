0f66b3c0a6769c12fcf523351144891ed8d255a9
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-15 09:14:52 -0800

    rename 'math_test' file to 'tabs_test'

---
+++ src/js/_tabs_test.js
@@ -0,0 +1,19 @@
+( function() {
+    'use strict';
+
+    var assert = require( './assert' );
+
+    describe( 'Tabs', function() {
+
+        it( '', function() {
+
+            // var div = document.createElement( 'div' );
+            // div.innerHTML = 'This is an example.';
+            // document.body.appendChild( div );
+            // div.parentNode.removeChild( div );
+
+        } );
+
+    } );
+
+} )();
