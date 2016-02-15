4e03a15023296bc3c7c1e403bd19f76e69d8b37d
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-14 18:52:09 -0800

    refactored example test into modules

--- src/_addition_test.js
+++ src/_addition_test.js
@@ -1,14 +1,16 @@
 ( function() {
     'use strict';
 
+    var addition = require( './addition.js' );
+
     describe( 'Addition', function() {
 
         it( 'adds positive numbers', function() {
-            assertEqual( add( 3, 4 ), 7 );
+            assertEqual( addition.add( 3, 4 ), 7 );
         } );
 
         it( 'uses IEEE 754 floating point', function() {
-            assertEqual( add( 0.1, 0.2 ), 0.30000000000000004 );
+            assertEqual( addition.add( 0.1, 0.2 ), 0.30000000000000004 );
         } );
 
         function assertEqual( actual, expected ) {
@@ -19,7 +21,4 @@
 
     } );
 
-    function add( a, b ) {
-        return a + b;
-    }
 } )();
---
+++ src/addition.js
@@ -0,0 +1,7 @@
+( function() {
+    'use strict';
+
+    exports.add = function add( a, b ) {
+        return a + b;
+    };
+} )();
