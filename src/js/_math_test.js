ee36764c846a56844dcd575eee2edaea416ca992
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-15 09:00:22 -0800

    setup barebones tab app

--- src/js/_math_test.js
+++ src/js/_math_test.js
@@ -1,25 +1,11 @@
 ( function() {
     'use strict';
 
-    var math = require( './math.js' );
     var assert = require( './assert' );
 
-    describe( 'Addition', function() {
+    describe( 'Something', function() {
 
-        it( 'adds positive numbers', function() {
-            assert.equal( math.add( 3, 4 ), 7 );
-        } );
-
-        it( 'uses IEEE 754 floating point', function() {
-            assert.equal( math.add( 0.1, 0.2 ), 0.30000000000000004 );
-        } );
-
-    } );
-
-    describe( 'Subtraction', function() {
-
-        it( 'subtracts positive numbers', function() {
-            assert.equal( math.subtract( 10, 3 ), 7 );
+        it( 'something', function() {
         } );
 
     } );
--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,4 @@
 ( function() {
+    'use strict';
 
-    var math = require( './math.js' );
-
-    console.log( 'Hello' );
-
-    console.log( '42 + 13 = ' + math.add( 42, 13 ) );
 } )();
