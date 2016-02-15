ca913c680f55f6b550b97e644c957f8954cd5066
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-15 08:41:55 -0800

    rename 'addition' to 'math'

--- src/js/_math_test.js
+++ src/js/_math_test.js
@@ -1,17 +1,17 @@
 ( function() {
     'use strict';
 
-    var addition = require( './addition.js' );
+    var math = require( './math.js' );
     var assert = require( './assert' );
 
     describe( 'Addition', function() {
 
         it( 'adds positive numbers', function() {
-            assert.equal( addition.add( 3, 4 ), 7 );
+            assert.equal( math.add( 3, 4 ), 7 );
         } );
 
         it( 'uses IEEE 754 floating point', function() {
-            assert.equal( addition.add( 0.1, 0.2 ), 0.30000000000000004 );
+            assert.equal( math.add( 0.1, 0.2 ), 0.30000000000000004 );
         } );
 
     } );
@@ -19,7 +19,7 @@
     describe( 'Subtraction', function() {
 
         it( 'subtracts positive numbers', function() {
-            assert.equal( addition.subtract( 10, 3 ), 7 );
+            assert.equal( math.subtract( 10, 3 ), 7 );
         } );
 
     } );
