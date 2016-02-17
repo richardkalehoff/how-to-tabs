61fdc9f83feb1871e2f4406f625b0086a0213679
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-17 08:51:04 -0800

    convert Tabs API to accept an options object

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -1,10 +1,27 @@
 ( function() {
     'use strict';
 
-    exports.init = function init( elementList, className ) {
-        elementList.forEach( function( element ) {
-            element.classList.add( className );
+    exports.init = function init( options ) {
+        var content = options.content,
+            defaultElement = options.default,
+            contentHideClass = options.contentHideClass;
+
+        if ( content === undefined ) {
+            throw new Error( 'Expected options.content' );
+        }
+
+        if ( defaultElement === undefined ) {
+            throw new Error( 'Expected options.default' );
+        }
+
+        if ( contentHideClass === undefined ) {
+            throw new Error( 'Expected options.contentHideClass' );
+        }
+
+        content.forEach( function( element ) {
+            element.classList.add( contentHideClass );
         } );
+        defaultElement.classList.remove( contentHideClass );
     };
 
 } )();
