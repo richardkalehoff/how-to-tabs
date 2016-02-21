e0ae8f97fc1dd001224c1dffeaac9a736581af18
Richard Kalehoff <richardkalehoff@gmail.com>
2016-02-21 08:04:29 -0800

    refactor Tabs library with handleClicks function

--- src/js/tabs.js
+++ src/js/tabs.js
@@ -8,14 +8,17 @@
         checkOption( options.activeTabClass, 'options.activeTabClass' );
         checkOption( options.hiddenContentClass, 'options.hiddenContentClass' );
 
+        handleClicks( options );
         showTab( options.defaultTab, options );
+    };
 
+    function handleClicks( options ) {
         options.tabs.forEach( function( tab ) {
             tab.addEventListener( 'click', function( event ) {
                 showTab( event.target, options );
             } );
         } );
-    };
+    }
 
     function showTab( tabToShow, options ) {
         var activeIndex = findIndex( options.tabs, tabToShow );
